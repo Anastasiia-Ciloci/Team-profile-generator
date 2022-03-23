3ba7ceba881aa0f58ee819fe7956dada5e56a47e
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-03-23 01:51:57 -0400

    emploeeprompt function

--- index.js
+++ index.js
@@ -1,18 +1,89 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+//const question = require("./src/questions");
+//const generateHtml = require("./src/generateHtml");
 
-function renderTeamHTML(fileName, data) {
-  fs.renderTeamHTML(fileName, data, (err) => {
-    err
-      ? console.error(err)
-      : console.log("Success! Here is your rendered html");
-  });
-}
+var team = [];
+
+const employeeType = () => {
+  return inquirer.prompt([
+    {
+      type: "rawlist",
+      name: "type",
+      message: "What type of employee would you like to add?",
+      choices: ["Engineer", "Manager", "Intern", "Finish building the team"],
+    },
+  ]);
+  /*.then((employee) => {
+      console.log("added employee type: " + employee.type);
+    });*/
+};
+
+const employeePrompt = () => {
+  return inquirer.prompt([
+    {
+      type: " input",
+      message: "What is employee name?",
+      name: "name",
+    },
+    {
+      type: " input",
+      message: "What is employee id?",
+      name: "id",
+    },
+    {
+      type: " input",
+      message: "What is employee email?",
+      name: "email",
+    },
+  ]);
+};
+
+const mainPrompt = (type) => {
+  if (type === "Engineer") {
+    return employeePrompt()
+      .then((employee) => {
+        //console.log(employee);
+        return inquirer
+          .prompt([
+            {
+              type: " input",
+              message: "What is your github user name?",
+              name: "github",
+            },
+          ])
+          .then((answer) => {
+            employee.github = answer.github;
+            return employee;
+          });
+      })
+      .then((employee) => {
+        team.push(
+          new Engineer(
+            employee.name,
+            employee.id,
+            employee.email,
+            employee.github
+          )
+        );
+      });
+  } else if (type === "Manager") {
+    console.log("Enter a manager");
+  } else if (type === "Intern") {
+    console.log("Enter an intern");
+  }
+};
 
 function init() {
-  inquirer.prompt(question).then((answers) => {
-    renderTeamHTML("./dist/my_team.html", generateHTML(answers));
+  employeeType().then((employee) => {
+    mainPrompt(employee.type)
+      .then(() => {
+        console.log(team);
+      })
+      .then(init);
   });
+  //console.log(team);
 }
 
 init();
--- src/questions.js
+++ src/questions.js
@@ -1,74 +1,59 @@
-const Employee = require("../lib/Employee");
+//const employee = require("../lib/Employee");
 
-const generalQ = 
-  [
-    {
-        type:" input",
-        message: "What is your name?",
-        name:"employeeName",
-    },
-    {
-        type:" input",
-        message: "What is the id number?",
-        name:"employeeId",
-    },
-    {
-        type:" input",
-        message: "What is your email?",
-        name:"employeeEmail",
-    }
+const generalQs = [
+  {
+    type: " input",
+    message: "What is your name?",
+    name: "employeeName",
+  },
+  {
+    type: " input",
+    message: "What is the id number?",
+    name: "employeeId",
+  },
+  {
+    type: " input",
+    message: "What is your email?",
+    name: "employeeEmail",
+  },
 ];
-const managersQs= [
-    {
-        type:" input",
-        message: "What is your office number?",
-        name:"managerOfficeN",
-    }
-]
+const managersQs = [
+  {
+    type: " input",
+    message: "What is your office number?",
+    name: "managerOfficeN",
+  },
+];
 
-const engineersQs=[
-    {
-        {
-            type:" input",
-            message: "What is your github user name?",
-            name:"gitHubName",
-        }
-    } 
-]
+const engineersQs = [
+  {
+    type: " input",
+    message: "What is your github user name?",
+    name: "gitHubName",
+  },
+];
 
-const internsQs= [
-    {
-        type:" input",
-        message: "What school did you finish ?",
-        name:"internSchool",
-    }
-]
+const internsQs = [
+  {
+    type: " input",
+    message: "What school did you finish ?",
+    name: "internSchool",
+  },
+];
 
-inquirer
-.prompt([
-    {
-        type:"rawlist",
-        name:"type",
+const employeePrompt = (type) => {
+  return inquirer
+    .prompt([
+      {
+        type: "rawlist",
+        name: "type",
         message: "What type of employee would you like to add?",
-        choices: ["Engineer", "Manager","Intern","Finish building the team"],
-    },
-     ...generalQ, 
-    
+        choices: ["Engineer", "Manager", "Intern", "Finish building the team"],
+      },
+    ])
+    .then((employee) => {
+      console.log("added employee type: " + employee.type);
+    });
+};
 
-])
-.then((employee) =>{
-if (employee.type ==="Engineer"){
-
-}
-if (employee.type ==="Manager"){
-
-}
-if (employee.type ==="Intern"){
-
-}
-if (employee.type ==="Finish building the team"){
-process.exit();
-}
-})
-
-module.exports = renderTeamHTML;
+module.exports = employeePrompt;
